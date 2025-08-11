590e6ae39188b33e5c16b2fc451f1cc5d83cdf64
leemongsho <mongsho.github@proton.me>
2025-08-10 22:27:57 -0700

    Update Gemini_get_started.py

--- Gemini_get_started.py
+++ Gemini_get_started.py
@@ -7,7 +7,7 @@ Also make sure your python is at least v3.9 or above
 """
 
 # This is the correct way to configure your API key.
-# It automatically picks up the GEMINI_API_KEY environment variable.
+# It automatically picks up the YOUR_API_KEY environment variable.
 genai.configure(api_key=os.environ["YOUR_API_KEY"]) #Insert your environment Gemini API Key
 
 # You then directly create an instance of the model you want to use.
@@ -18,4 +18,5 @@ user_prompt = "Describe in 100 words or less the key features of gemini-2.5-flas
 
 # You can now use the model instance to generate content.
 response = model.generate_content(user_prompt)
+
 print(response.text)
